41592dfa3da7ed9109d8cfe320b88bff5d54abea
Forvisk <adrianojr11@gmail.com>
2017-08-09 09:33:11 -0300

    correçao ex1_b

--- Exercicios/aula02_b.c
+++ Exercicios/aula02_b.c
@@ -42,23 +42,24 @@ void multiplicaMatriz( int **m1, int **m2, int **mR){
 			for( j2 = 0; j2 < n; j2++){
 				k += m1[i1][l]*m2[l][j2];
 			}
+			mR[i1][l] = k;
 		}
-		mR[i1][l] = k;
 	}
-	
+	/*
 	for( i1 = 0; i1 < n; i1++){
 		for( l = 0; l < n; l++){
 			printf("%i ", mR[i1][l]);			
 		}
 		printf("\n");
 	}
+	*/
 }
 
 
 
 void main(){
 	int **m1, **m2, **mR;
-	scanf("%i", n);
+	scanf("%i", &n);
 	
 	m1 = (int **)malloc(n *sizeof( int*));
 	m2 = (int **)malloc(n *sizeof( int*));
