ad268583afd2b50918c225578a6a80475301a2bf
Adriano Zanella Jr <adrianojr11@gmail.com>
2017-08-25 16:23:03 -0300

    MErge sort não funciona

--- Trabalho1/ordenacao.c
+++ Trabalho1/ordenacao.c
@@ -1,12 +1,12 @@
-/*Trabalho 1 - Ordenacao
-* @autor Adriano Zanella Junior
-* Inicio em 24/08/2017
-* Fim em //
-*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/time.h>
-#include <Windows.h>
+	/*Trabalho 1 - Ordenacao
+	* @autor Adriano Zanella Junior
+	* Inicio em 24/08/2017
+	* Fim em //
+	*/
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <sys/time.h>
+	#include <Windows.h>
 
 LARGE_INTEGER inicio, fim, tempo, frequencia;
 
@@ -42,17 +42,17 @@ int main(){
 	srand((unsigned) time(&t));
 
 	printf("Iniciando Ordenacoes\n");
-	//printf("%i", 7 / 3);
+		printf("%i", (5 - 2) / 2);
 	vet = malloc( tam0 * sizeof(int));
-	/*
-	populaVetor( vet, tam0, 1);
-	populaVetor( vet, tam0, 2);
-	populaVetor( vet, tam0, 3);
-	populaVetor( vet, tam0, 4);
-	*/
-	//bubbleSort( vet);
-	//printf("\n...\n");
-	//insertSort(vet);
+		/*
+		populaVetor( vet, tam0, 1);
+		populaVetor( vet, tam0, 2);
+		populaVetor( vet, tam0, 3);
+		populaVetor( vet, tam0, 4);
+		*/
+		//bubbleSort( vet);
+		//printf("\n...\n");
+		//insertSort(vet);
 
 	printf("\n Populando\n");
 	populaVetor( vet, 500, 3);
@@ -75,15 +75,16 @@ int main(){
 		printf(" %i ", vet[i]);
 	free(vet);
 
-	/*	//Deixamos o merge para depois
+	/*
+	//Deixamos o merge para depois
 	printf("\n Populando\n");
-	populaVetor( vet, 500, 3);
-	for( i = 0; i < 500; i++)
+	populaVetor( vet, 10, 3);
+	for( i = 0; i < 10; i++)
 		printf(" %i ", vet[i]);
 	printf("Merge Sort\n");
-	merge(vet, 0, 500);
+	merge(vet, 0, 10);
 	printf("\n Ordenado\n");
-	for( i = 0; i < 500; i++)
+	for( i = 0; i < 10; i++)
 		printf(" %i ", vet[i]);
 	*/
 
@@ -105,15 +106,15 @@ void finalizaRelogio(){
 	tempo.QuadPart *= 1000000;
 	tempo.QuadPart /= frequencia.QuadPart;
 	printf("Tempo da ordenacao: %li us\n" , tempo );
-	//printf("Relogio parado... Pressione qualquer tecla para continuar\n" );
-	//getchar();
+		//printf("Relogio parado... Pressione qualquer tecla para continuar\n" );
+		//getchar();
 	printf("\n...\n");
 }
 
-//retirado do exemplo de bubble sort
+	//retirado do exemplo de bubble sort
 int time_diff(struct timeval x , struct timeval y){
 	int diff;
-	diff = (y.tv_sec - x.tv_sec) + 1e-6 * (y.tv_usec - x.tv_usec); /* in seconds */
+		diff = (y.tv_sec - x.tv_sec) + 1e-6 * (y.tv_usec - x.tv_usec); /* in seconds */
 	return diff;
 }
 
@@ -121,43 +122,43 @@ int populaVetor( int *vet, int tam, int tipo){
 	int i;
 	printf("Iniciando vetor de %i numeros ", tam);
 
-	//vet = malloc( tam * sizeof(int));
+		//vet = malloc( tam * sizeof(int));
 	switch (tipo){
 		case 1:
-			printf("crescente.\n");
-			for( i = 0; i < tam; i++){
-				vet[i] = i+1;
-			}
-			break;
+		printf("crescente.\n");
+		for( i = 0; i < tam; i++){
+			vet[i] = i+1;
+		}
+		break;
 		case 2:
-			printf("decrescente.\n");
-			for( i = tam-1; i >= 0; i--){
-				vet[i] = i+1;
-			}
-			break;
+		printf("decrescente.\n");
+		for( i = tam-1; i >= 0; i--){
+			vet[i] = i+1;
+		}
+		break;
 		case 3:
-			printf("aleatorio.\n");
-			for(i = 0; i < tam; i++){
-				vet[i] = rand() % 1000;
-			}
-			break;
+		printf("aleatorio.\n");
+		for(i = 0; i < tam; i++){
+			vet[i] = rand() % 1000;
+		}
+		break;
 		case 4:
-			printf("aleatorio com 100000000.\n");
-			for(i = 0; i < tam; i++){
-				vet[i] = rand() % 1000;
-			}
-			i = rand() % tam;
-			vet[i] = 100000000;
-			break;
+		printf("aleatorio com 100000000.\n");
+		for(i = 0; i < tam; i++){
+			vet[i] = rand() % 1000;
+		}
+		i = rand() % tam;
+		vet[i] = 100000000;
+		break;
 		default:
-			printf("\nErro!\n");
-			return 0;
+		printf("\nErro!\n");
+		return 0;
 	}
-	/*
-	for( i = 0; i < 100; i++)
-		printf("%i ", vet[i]);
-	printf("\n");
-	*/
+		/*
+		for( i = 0; i < 100; i++)
+			printf("%i ", vet[i]);
+		printf("\n");
+		*/
 	return 1;
 }
 
@@ -258,38 +259,61 @@ void insert( int *vet, int tam){
 void mergeSort( int *vet);
 
 int merge( int *vet, int ini, int fim){
-	if( ini >= fim)
-		return 0;
 	int meio = (fim - ini) / 2;
+	if( ini < 2)
+	printf("\n Merge %i - %i : %i", ini, fim, meio);
+	if( ini >= fim){
+		//printf("Saida");
+		return 0;
+	}
 	merge( vet, ini, meio);
-	merge( vet, meio+1, fim); 
+	merge( vet, meio+1, fim);
 	doMerge( vet, ini, meio, fim);
+	return 0;
 }
 
 void doMerge( int *vet, int ini, int meio, int fim){
+	printf("doMerge %i : %i : %i", ini, meio, fim);
 	int i, j, k;
 	int *aux;
 	aux = malloc((fim - ini +1) * sizeof(int));
 
 	i = ini;
-	j = meio+1;
+	j = meio;
 	k = 0;
-	while( (i < meio+1) && (j < fim) && (k < fim-ini+1)){
-		if( vet[i] < vet[j]){
-			aux[k] = vet[i];
-			i++;
+	while( ((i < meio) || (j < fim)) && (k < fim-ini+1)){
+		if( i < meio){
+			if( j < fim){
+				if( vet[i] < vet[j]){
+					aux[k] = vet[i];
+					i++;
+				}else if(vet[i] > vet[j]){
+					aux[k] = vet[j];
+					j++;
+				}else{
+					aux[k] = vet[i];
+					i++;
+				}
+			}else{
+				aux[k] = vet[i];
+				i++;
+			}
 		}else{
-			aux[k] = vet[j];
-			j++;
+			if( j < fim){
+				aux[k] = vet[j];
+				j++;
+			}
 		}
 		k++;
 	}
 	i = ini;
 	k = 0;
+	printf("\n");
 	while((i < fim) && ( k < fim-ini+1)){
 		vet[i] = aux[k];
 		i++;
 		k++;
+		printf("%i ", vet[i]);
 	}
 	free(aux);
 }
